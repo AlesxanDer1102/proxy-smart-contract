97b6fcd7d8d3314d7b5048ab121e316e197abae5
AlesxanDer1102 <diego.orrego.t@uni.pe>
2024-12-29 01:59:39 -0500

    update BOxV2 to UUPSUpgradeable only

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -2,21 +2,9 @@
 pragma solidity ^0.8.24;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
-    
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(address newOwner) public initializer {
-        __Ownable_init(newOwner);
-        __UUPSUpgradeable_init();
-    }
 
     function setNumber(uint256 _number) external {
         number = _number;
