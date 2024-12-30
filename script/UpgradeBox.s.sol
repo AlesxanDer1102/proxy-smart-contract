6b66a33495a4d7efdbecdfbc306ae489c2db2391
AlesxanDer1102 <diego.orrego.t@uni.pe>
2024-12-30 02:57:27 -0500

    fix some bugs

--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -6,16 +6,15 @@ import {BoxV1} from "../src/BoxV1.sol";
 import {BoxV2} from "../src/BoxV2.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 contract UpgradeBox is Script {
     function run() external returns (address) {
-        address mostRecentlyDeploy = DevOpsTools.get_most_recent_deployment("ERC1967Proxy", block.chainid);
+        address mostRecentlyDeployedProxy = DevOpsTools.get_most_recent_deployment("ERC1967Proxy", block.chainid);
 
         vm.startBroadcast();
         BoxV2 newBox = new BoxV2();
         vm.stopBroadcast();
-        address proxy  = upgradeBox(mostRecentlyDeploy,address(newBox));
+        address proxy  = upgradeBox(mostRecentlyDeployedProxy,address(newBox));
         return proxy;
     }
 
